6c1077a93795ae549f39a851fed5540f575cf988
Erick Tucto <erick@ericktucto.com>
2025-07-23 15:41:21 -0500

    change: nuevos estilos para buscador

--- src/components/organisms/TopActionMap.vue
+++ src/components/organisms/TopActionMap.vue
@@ -27,6 +27,7 @@ interface ResultInput extends TypeResult {
 
 const resultInput = ref<ResultInput[]>([]);
 const search = ref('');
+const active = ref(false);
 const searchinput = ref<HTMLInputElement|null>(null);
 const loading = ref({
   nominatim: false,
@@ -53,21 +54,19 @@ async function onSearch(event: Event) {
   const target = (event.target as HTMLInputElement);
   _search(target.value.trim().toLowerCase());
 }
-function onClickDirection(e: Event, r: DirectionSearch) {
-  const div = (e.target as HTMLDivElement);
+function onClickDirection(r: DirectionSearch) {
   const result = (r as DirectionSearch);
   emit('showto', {
     lat: Number(result.lat),
     lng: Number(result.lon),
     type: 'direccion',
   });
-  div.blur();
+  active.value = false;
 }
-function onClickResult(e: Event, r: ResultInput) {
-  const div = (e.target as HTMLDivElement);
+function onClickResult(r: ResultInput) {
   const result = (r as ResultInput);
   emit('showto', { id: result.id, type: 'revisita' });
-  div.blur();
+  active.value = false;
 }
 interface DirectionSearch extends TypeResult {
   display_name: string;
@@ -90,32 +89,28 @@ async function onClickSearch() {
 function onFocusInputSearch() {
   emit('focusinput')
   _search(search.value);
+  active.value = true;
 }
 function onMyLocation() {
   emit('myLocation');
-  blurComponent();
+  active.value = false;
 }
-function onBack(e: Event) {
-  const btn = (e.target as HTMLButtonElement);
-  btn.blur();
-  blurComponent();
-}
-function blurComponent() {
-  searchinput.value?.focus();
-  searchinput.value?.blur();
+function onBack() {
+  active.value = false;
 }
 onMounted(() => {
   _search("");
 });
 </script>
 <template>
-  <div class="absolute top-0 z-1000 w-full max-h-[100dvh]
-      focus-within:h-dvh
-      focus-within:bg-white
-    "
+  <div class="absolute top-0 z-1000 w-full max-h-[100dvh]"
+    :class="active ? ['h-dvh', 'bg-white'] : []"
   >
-    <div class="flex gap-2">
-      <div class="flex-row-reverse flex-1 gap-1 flex rounded-full bg-white h-12 p-2 shadow-md focus:outline-none">
+    <div class="flex gap-2 px-2 py-4">
+      <div
+        class="flex-row-reverse flex-1 gap-1 flex rounded-full bg-white p-2 focus:outline-none"
+        :class="active ? ['border border-gray-200'] : ['shadow-md']"
+      >
         <input
           ref="searchinput"
           v-model="search"
@@ -127,35 +122,28 @@ onMounted(() => {
         />
         <button
           type="button"
-          class="bg-white p-1 w-8 h-8 place-items-center peer-focus-within:hidden"
-          @click="() => searchinput?.focus()"
+          class="bg-white p-1 w-8 h-8 place-items-center"
+          @click="() => active ? onBack() : searchinput?.focus()"
         ><HugeiconsIcon
-          :icon="Location01Icon"
-        /></button>
-        <button
-          type="button"
-          class="hidden bg-white p-1 w-8 h-8 place-items-center peer-focus-within:grid"
-          @click="onBack"
-          @focus="onBack"
-        ><HugeiconsIcon
-          :icon="ArrowLeft02Icon"
+          :icon="active ? ArrowLeft02Icon : Location01Icon"
         /></button>
       </div>
       <button
         type="button"
-        class="p-2 rounded-full bg-white w-12 h-12 grid place-items-center shadow-md"
+        class="p-2 rounded-full bg-white w-12 h-12 grid place-items-center"
+        :class="active ? ['border border-gray-200'] : ['shadow-md']"
       ><HugeiconsIcon
         class="w-6 h-6"
         :icon="Gps01Icon"
         @click="onMyLocation"
       /></button>
     </div>
-    <div class="resultsearch hidden mt-2">
+    <div class="grid" :class="{ 'hidden': !active }">
       <ResultDirection
         v-for="(d, index) in directions"
         :key="`direction-${index}`"
         :text="d.display_name"
-        @focus="(e: Event) => onClickDirection(e, d)"
+        @focus="onClickDirection(d)"
         class="border-gray-200 border-t"
         :class="{
           'border-y': index === directions.length - 1 && resultInput.length === 0,
@@ -176,7 +164,7 @@ onMounted(() => {
         :key="`result-${r.id}`"
         :text="r.text"
         :details="r.details"
-        @focused="(e) => onClickResult(e, r)"
+        @focused="onClickResult(r)"
         class="border-gray-200 border-t"
         :class="{
           'border-y': index === resultInput.length - 1,
@@ -185,9 +173,3 @@ onMounted(() => {
     </div>
   </div>
 </template>
-<style scoped>
-.absolute:focus-within .resultsearch.hidden {
-  display: grid;
-  overflow-y: scroll;
-}
-</style>
